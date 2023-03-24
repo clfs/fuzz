4e7d2fda172b1ba4c1aa64c20ea468f79152258c
Calvin Figuereo-Supraner <mail@calvin.page>
2023-03-24 09:18:24 -0700

    Change package naming for protobufold

--- projects/protobufold/fuzz_test.go
+++ projects/protobufold/fuzz_test.go
@@ -4,14 +4,14 @@ package protobufold_fuzz
 import (
 	"testing"
 
-	proto_deprecated "github.com/golang/protobuf/proto" //lint:ignore SA1019 fuzzing despite deprecation
-	anypb_deprecated "github.com/golang/protobuf/ptypes/any"
+	"github.com/golang/protobuf/proto" //lint:ignore SA1019 fuzzing despite deprecation
+	anypb "github.com/golang/protobuf/ptypes/any"
 )
 
 // FuzzUnmarshal ensures proto.Unmarshal does not panic.
 func FuzzUnmarshal(f *testing.F) {
 	f.Fuzz(func(t *testing.T, b []byte) {
-		var pb anypb_deprecated.Any
-		_ = proto_deprecated.Unmarshal(b, &pb)
+		var pb anypb.Any
+		_ = proto.Unmarshal(b, &pb)
 	})
 }
