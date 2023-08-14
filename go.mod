b7f17a94c1d2e29e2a2893288814531468e2bbe2
Calvin Figuereo-Supraner <mail@calvin.page>
2023-08-13 19:19:18 -0700

    Add hcl tests

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21
 
 require (
 	github.com/BurntSushi/toml v1.3.2
+	github.com/hashicorp/hcl/v2 v2.17.0
 	github.com/pelletier/go-toml/v2 v2.0.9
 	golang.org/x/image v0.11.0
 	google.golang.org/protobuf v1.31.0
@@ -11,6 +12,10 @@ require (
 )
 
 require (
+	github.com/agext/levenshtein v1.2.1 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/zclconf/go-cty v1.13.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 )
--- hcl/hcl_test.go
+++ hcl/hcl_test.go
@@ -1,2 +1,40 @@
 // Package hcl contains HCL fuzzing tests.
 package hcl
+
+import (
+	"testing"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/json"
+)
+
+func FuzzJSONParseWithStartPos(f *testing.F) {
+	f.Fuzz(func(t *testing.T, b []byte, l, c int) {
+		_, _ = json.ParseWithStartPos(b, "fuzz.hcl", hcl.Pos{Line: l, Column: c})
+	})
+}
+
+func FuzzParseTemplate(f *testing.F) {
+	f.Fuzz(func(t *testing.T, b []byte, l, c int) {
+		_, _ = hclsyntax.ParseTemplate(b, "fuzz.hcl", hcl.Pos{Line: l, Column: c})
+	})
+}
+
+func FuzzParseTraversalAbs(f *testing.F) {
+	f.Fuzz(func(t *testing.T, b []byte, l, c int) {
+		_, _ = hclsyntax.ParseTraversalAbs(b, "fuzz.hcl", hcl.Pos{Line: l, Column: c})
+	})
+}
+
+func FuzzParseExpression(f *testing.F) {
+	f.Fuzz(func(t *testing.T, b []byte, l, c int) {
+		_, _ = hclsyntax.ParseExpression(b, "fuzz.hcl", hcl.Pos{Line: l, Column: c})
+	})
+}
+
+func FuzzParseConfig(f *testing.F) {
+	f.Fuzz(func(t *testing.T, b []byte, l, c int) {
+		_, _ = hclsyntax.ParseConfig(b, "fuzz.hcl", hcl.Pos{Line: l, Column: c})
+	})
+}
